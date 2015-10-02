ba42ac91e2aa6b2f341ac059ed32ee71e8eb203c
Yansen <chenciensen@gmail.com>
2015-10-01 22:56:54 -0400

    Remove default migration

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
+            'database'  => env('DB_DATABASE', 'a_commerce'),
+            'username'  => env('DB_USERNAME', 'yansen'),
+            'password'  => env('DB_PASSWORD', 'password!!123'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
